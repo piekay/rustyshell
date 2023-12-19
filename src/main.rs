1ac9d49922c1668302eeecb39633d0565fbbfad0
piekay <82611831+piekay@users.noreply.github.com>
2023-12-19 17:34:12 +0100

    fix: code cleanup fix: tilde support is now global

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::fmt::Error;
+use dirs::home_dir;
 use rustyline::{DefaultEditor};
 use rustyline::error::ReadlineError;
 use crate::command_handler::command_handler;
@@ -8,9 +9,9 @@ mod config_handler;
 mod command_handler;
 
 fn main() -> Result<(), Error> {
-    let mut rl = DefaultEditor::new().expect("Couldn't load Editor");
+    let mut rl = DefaultEditor::new().expect("Couldn't load Readline Editor");
 
-    if rl.load_history(&dirs::home_dir().unwrap().join(".rsh_history")).is_err() {
+    if rl.load_history(&home_dir().unwrap().join(".rsh_history")).is_err() {
         println!("No history file found, will create one on exit")
     }
     loop {
@@ -21,8 +22,9 @@ fn main() -> Result<(), Error> {
                     if line.trim() == "exit" {
                         break;
                     }
+                    let tilde_handler = line.replace("~", &*home_dir().unwrap().to_string_lossy());
                     rl.add_history_entry(line.as_str()).expect("TODO: Couldn't add to history");
-                    command_handler(line);
+                    command_handler(tilde_handler);
                 },
                 Err(ReadlineError::Interrupted) => {
                     println!("CTRL-C");
@@ -39,6 +41,6 @@ fn main() -> Result<(), Error> {
             }
         }
     }
-    rl.save_history(&dirs::home_dir().unwrap().join(".rsh_history")).expect("TODO: panic message");
+    rl.save_history(&home_dir().unwrap().join(".rsh_history")).expect("Couldn't write to history file");
     Ok(())
 }
